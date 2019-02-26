dafe9e83c9e2608728821cb72e4781443ab71aa2
Oni Oyewole <onioyewole1910@gmail.com>
2019-02-27 00:39:11 +0100

    First Commit

---
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -0,0 +1,37 @@
+﻿using IdentityModel.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace BankOfDotNet.ConsoleClient
+{
+    class Program
+    {
+        public static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        private static async Task MainAsync()
+        {
+            // discover all endpoints using metadata of identity server
+            var disco = await DiscoveryClient.GetAsync("http://localhost:5000");
+
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            //Grab a bearer token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("bankOfDotnetApi");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+        }
+    }
+}
