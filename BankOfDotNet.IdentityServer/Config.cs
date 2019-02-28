bf1b13a6c925533749ede252482bb2f5dd2f302e
Oni Oyewole <ooni@eminenttechnology.com>
2019-02-28 19:38:49 +0100

    Resource Owner Started

--- BankOfDotNet.IdentityServer/Config.cs
+++ BankOfDotNet.IdentityServer/Config.cs
@@ -1,4 +1,5 @@
 ﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +9,25 @@ namespace BankOfDotNet.IdentityServer
 {
     public class Config
     {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username ="Oni",
+                    Password = "password"
+                },
+
+                new TestUser
+                {
+                    SubjectId ="2",
+                    Username ="Oyewole",
+                    Password = "password"
+                }
+            };
+        }
         public static IEnumerable<ApiResource> GetAllApiResources()
         {
             return new List<ApiResource>
