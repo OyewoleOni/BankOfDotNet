df2b761cc9a8c3e846ba8afb37ef6a5861b794d8
oni oyewole <ooni@eminenttechnology.com>
2019-03-08 19:39:41 +0100

    Implicit Flow Ends

--- BankOfDotNet.IdentityServer/Config.cs
+++ BankOfDotNet.IdentityServer/Config.cs
@@ -1,4 +1,5 @@
-﻿using IdentityServer4.Models;
+﻿using IdentityServer4;
+using IdentityServer4.Models;
 using IdentityServer4.Test;
 using System;
 using System.Collections.Generic;
@@ -83,7 +84,14 @@ namespace BankOfDotNet.IdentityServer
                     ClientName ="MVC  Client",
                     AllowedGrantTypes = GrantTypes.Implicit,
 
-                    RedirectUris = {"http://localhost:5003/signin-oidc"}
+                    RedirectUris = {"http://localhost:5003/signin-oidc"},
+                    PostLogoutRedirectUris = {"http://localhost:5003/signout-callback-oidc"},
+
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile
+                    }
                 }
             };
         }
